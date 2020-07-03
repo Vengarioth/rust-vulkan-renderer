19193c233e0cc64d8e9b76bedce98c3ce202e2e3
Vengarioth | Andreas Fischer <opensource@deviru.de>
2020-07-03 22:21:28 +0200

    use serde feature of tinypath

--- rvrc/src/assets/asset.rs
+++ rvrc/src/assets/asset.rs
@@ -18,17 +18,17 @@ impl Asset {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ShaderAsset {
-    pub vertex_shader: String,
+    pub vertex_shader: Path,
     pub vertex_shader_entry_point: String,
-    pub fragment_shader: String,
+    pub fragment_shader: Path,
     pub fragment_shader_entry_point: String,
 }
 
 impl ShaderAsset {
     pub fn get_dependencies(&self, file_path: &Path) -> Result<Vec<Path>, Error> {
         Ok(vec![
-            Path::from_str(&self.vertex_shader).unwrap().relative_to(file_path),
-            Path::from_str(&self.fragment_shader).unwrap().relative_to(file_path),
+            self.vertex_shader.relative_to(file_path),
+            self.fragment_shader.relative_to(file_path),
         ])
     }
 }
--- rvrc/src/builder/shader/mod.rs
+++ rvrc/src/builder/shader/mod.rs
@@ -13,8 +13,8 @@ pub use compile_error::*;
 
 pub fn build_shader_asset(asset: &ShaderAsset, context: &mut BuildContext) -> Result<(), Error> {
 
-    let vertex_shader_source = context.load(&Path::from_str(&asset.vertex_shader)?)?;
-    let fragment_shader_source = context.load(&Path::from_str(&asset.fragment_shader)?)?;
+    let vertex_shader_source = context.load(&asset.vertex_shader)?;
+    let fragment_shader_source = context.load(&asset.fragment_shader)?;
 
     let vertex_shader_source = std::str::from_utf8(&vertex_shader_source)?;
     let fragment_shader_source = std::str::from_utf8(&fragment_shader_source)?;
@@ -22,14 +22,14 @@ pub fn build_shader_asset(asset: &ShaderAsset, context: &mut BuildContext) -> Re
     let vertex_shader_binary = compile(
         &asset.vertex_shader_entry_point,
         vertex_shader_source,
-        &context.get_full_path(&Path::from_str(&asset.vertex_shader)?).to_platform_string(),
+        &context.get_full_path(&asset.vertex_shader).to_platform_string(),
         shaderc::ShaderKind::Vertex,
     )?;
 
     let fragment_shader_binary = compile(
         &asset.fragment_shader_entry_point,
         fragment_shader_source,
-        &context.get_full_path(&Path::from_str(&asset.fragment_shader)?).to_platform_string(),
+        &context.get_full_path(&asset.fragment_shader).to_platform_string(),
         shaderc::ShaderKind::Fragment,
     )?;
 
